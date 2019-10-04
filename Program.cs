e6e09478972cdc5d235714cc98e3546a9eaf40a6
Matthew Yates <Mat.C.Yates@gmail.com>
2019-10-04 13:53:12 -0400

    added git ignore

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace titler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
