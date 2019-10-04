27f1eee0ca5065dcc2780fb17e91393b9600eaea
Matthew Yates <Mat.C.Yates@gmail.com>
2019-10-04 15:37:56 -0400

    initial project complete

--- Program.cs
+++ Program.cs
@@ -6,8 +6,22 @@ namespace titler
   {
     static void Main(string[] args)
     {
+      for (int i = 0; i < 101; i++)
+      {
+        Console.WriteLine($"{RandName()}");
+      }
+    }
 
-      Console.WriteLine("Hello World!");
+    private static string RandName()
+    {
+      var words = new Words();
+      var rand = new Random();
+      int adjectiveIndex = rand.Next(0, words.Adjectives.Length);
+      int nounIndex = rand.Next(0, words.Nouns.Length);
+      string name = $"{words.Adjectives[adjectiveIndex]} {words.Nouns[nounIndex]}";
+      return name;
     }
   }
 }
+
+// make words adjective and nouns members either fields or properties
